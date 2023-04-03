90eac6962130c7dd9edfa3bd067c1a0846b9228f
Evangelos Stavropoulos <evang.stavropoulos@gmail.com>
2023-04-03 10:33:15 +0200

    database operations moved to crud.py

---
+++ app/crud.py
@@ -0,0 +1,75 @@
+from sqlalchemy.orm import Session
+from app.schemas import TeamCreate, PlayerCreate, GameCreate
+from app.models import Team as DBTeam, Player as DBPlayer, Game as DBGame
+from fastapi import HTTPException
+
+
+def create_team(db: Session, team: TeamCreate):
+    if team.player1_name == team.player2_name:
+        raise HTTPException(status_code=400, detail="Player names must be unique")
+
+    existing_team = db.query(DBTeam).filter(DBTeam.name == team.name).first()
+    if existing_team:
+        db.close()
+        raise HTTPException(status_code=400, detail="Team name already in use")
+
+    db_team = DBTeam(name=team.name)
+    player1 = get_or_create_player(db, team.player1_name)
+    player2 = get_or_create_player(db, team.player2_name)
+
+    db_team.players.extend([player1, player2])
+    db.add(db_team)
+    db.commit()
+    db.refresh(db_team)
+
+    return db_team
+
+
+def get_teams(db: Session):
+    return db.query(DBTeam).all()
+
+
+def create_player(db: Session, player: PlayerCreate):
+    db_player = DBPlayer(name=player.name)
+    db.add(db_player)
+    db.commit()
+    db.refresh(db_player)
+    return db_player
+
+
+def get_players(db: Session):
+    return db.query(DBPlayer).all()
+
+
+def get_player_by_name(db: Session, player_name: str):
+    db_player = db.query(DBPlayer).filter(DBPlayer.name == player_name).first()
+    if db_player is None:
+        raise HTTPException(status_code=404, detail="Player not found")
+    return db_player
+
+
+def get_or_create_player(db: Session, player_name: str):
+    player = db.query(DBPlayer).filter(DBPlayer.name == player_name).first()
+    if not player:
+        player = DBPlayer(name=player_name)
+        db.add(player)
+        db.flush()
+    return player
+
+
+def create_game(db: Session, game: GameCreate):
+    db_game = DBGame(
+        team1_name=game.team1_name,
+        team2_name=game.team2_name,
+        team1_score=game.team1_score,
+        team2_score=game.team2_score,
+        date=game.date,
+    )
+    db.add(db_game)
+    db.commit()
+    db.refresh(db_game)
+    return db_game
+
+
+def get_games(db: Session):
+    return db.query(DBGame).all()
--- app/main.py
+++ app/main.py
@@ -1,97 +1,43 @@
 from fastapi import Depends, FastAPI
-from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 from app.schemas import TeamCreate, PlayerCreate, GameCreate, Team, Player, Game
-from app.models import Team as DBTeam, Player as DBPlayer, Game as DBGame, Base
-from app.database import engine, get_db
-
+from app.database import get_db
+from app import crud
 
 app = FastAPI()
-
-Base.metadata.create_all(bind=engine)
 
 
 @app.post("/teams/", response_model=Team)
 def create_team(team: TeamCreate, db: Session = Depends(get_db)):
-    if team.player1_name == team.player2_name:
-        raise HTTPException(status_code=400, detail="Player names must be unique")
-
-    # Check if a team with the given name already exists
-    existing_team = db.query(DBTeam).filter(DBTeam.name == team.name).first()
-    if existing_team:
-        db.close()
-        raise HTTPException(status_code=400, detail="Team name already in use")
-
-    db_team = DBTeam(name=team.name)
-
-    # Check if players exist, create them if not
-    player1 = db.query(DBPlayer).filter(DBPlayer.name == team.player1_name).first()
-    if not player1:
-        player1 = DBPlayer(name=team.player1_name)
-        db.add(player1)
-        db.flush()
-
-    player2 = db.query(DBPlayer).filter(DBPlayer.name == team.player2_name).first()
-    if not player2:
-        player2 = DBPlayer(name=team.player2_name)
-        db.add(player2)
-        db.flush()
-
-    db_team.players.extend([player1, player2])
-    db.add(db_team)
-    db.commit()
-    db.refresh(db_team)
-
-    return db_team
+    return crud.create_team(db, team)
 
 
 @app.get("/teams/", response_model=List[Team])
 def read_teams(db: Session = Depends(get_db)):
-    teams = db.query(DBTeam).all()
-    return teams
+    return crud.get_teams(db)
 
 
 @app.post("/players/", response_model=Player)
 def create_player(player: PlayerCreate, db: Session = Depends(get_db)):
-    db_player = DBPlayer(name=player.name)
-    db.add(db_player)
-    db.commit()
-    db.refresh(db_player)
-    return db_player
+    return crud.create_player(db, player)
 
 
 @app.get("/players/", response_model=List[Player])
 def read_players(db: Session = Depends(get_db)):
-    players = db.query(DBPlayer).all()
-    return players
+    return crud.get_players(db)
 
 
 @app.get("/players/{player_name}", response_model=Player)
 def read_player_by_name(player_name: str, db: Session = Depends(get_db)):
-    # Query the database for a player with the given name
-    db_player = db.query(DBPlayer).filter(DBPlayer.name == player_name).first()
-    if db_player is None:
-        raise HTTPException(status_code=404, detail="Player not found")
-    return db_player
+    return crud.get_player_by_name(db, player_name)
 
 
 @app.post("/games/", response_model=Game)
 def create_game(game: GameCreate, db: Session = Depends(get_db)):
-    db_game = DBGame(
-        team1_name=game.team1_name,
-        team2_name=game.team2_name,
-        team1_score=game.team1_score,
-        team2_score=game.team2_score,
-        date=game.date,
-    )
-    db.add(db_game)
-    db.commit()
-    db.refresh(db_game)
-    return db_game
+    return crud.create_game(db, game)
 
 
 @app.get("/games/", response_model=List[Game])
 def read_games(db: Session = Depends(get_db)):
-    games = db.query(DBGame).all()
-    return games
+    return crud.get_games(db)
