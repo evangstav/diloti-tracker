ff3d4fbc6d002a84b3519f8181e4cf792f45bf14
Evangelos Stavropoulos <evang.stavropoulos@gmail.com>
2023-04-07 16:11:10 +0200

    Update database connection to use env

--- app/database.py
+++ app/database.py
@@ -1,9 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from os import environ
 
-DATABASE_URL = "sqlite:///./game_scores.db"
+env = environ.get("ENV", "dev")
+HOSTNAME = environ.get("HOSTNAME")
+USER = environ.get("USER")
+PASSWORD = environ.get("PASSWORD")
+DB_NAME = environ.get("DB_NAME")
 
-engine = create_engine(DATABASE_URL)
+if env == "prod":
+    DATABASE_URL = f"mysql+pymysql://{USER}:{PASSWORD}@{HOSTNAME}/{DB_NAME}"
+else:
+    DATABASE_URL = "sqlite:///./game_scores.db"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
