63c14c144fd1c6a3b20939f4cd306db847ea6669
Evangelos Stavropoulos <evang.stavropoulos@gmail.com>
2023-04-10 14:46:06 +0200

    Added alembic migration script

---
+++ alembic/versions/601021ce6fef_change_keys_from_names_to_id_s_added_a_.py
@@ -0,0 +1,22 @@
+"""Change keys from names to id's. Added a Scores table
+
+Revision ID: 601021ce6fef
+Revises: 45c6d2df0d6e
+Create Date: 2023-04-10 14:43:07.118902
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "601021ce6fef"
+down_revision = "45c6d2df0d6e"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
